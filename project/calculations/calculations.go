1b8a5dadded28762375d2bc063506df471cfc16f
codr-alpha <pares31415@gmail.com>
2024-10-01 03:06:27 +0330

    making velocity and Calculate function in project/calculations/calculations.go adding some constants to project/structs_and_constants/structs_and_constants.go

--- project/calculations/calculations.go
+++ project/calculations/calculations.go
@@ -3,10 +3,10 @@ package calculations
 import (
 	"my_mod/project/structs_and_constants"
 	"math"
+	"time"
 )
 
-
-func Haversine(p1, p2 structs_and_constants.Point) float64 {
+func haversine(p1, p2 structs_and_constants.Point) float64 {
 	lat1 := p1.Lat * math.Pi / 180
 	lng1 := p1.Lng * math.Pi / 180
 	lat2 := p2.Lat * math.Pi / 180
@@ -20,4 +20,43 @@ func Haversine(p1, p2 structs_and_constants.Point) float64 {
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 
 	return structs_and_constants.R_earth * c
+}
+
+func velocity(dTime, dDistance float64) float64 {
+	if (dTime <= 0) {
+		return 100 + 1
+	}
+
+	return dDistance * (60 * 60) / dTime // km/h
+}
+
+
+func isBefore5am(t time.Time) bool {
+	hour := t.Hour()
+	minute := t.Minute()
+	second := t.Second()
+
+	return (hour * 60 * 60 + minute * 60 + second) <= 5 * 60 * 60 
+}
+
+func calculate(p1, p2 structs_and_constants.Point) float64 {
+	dTime := p2.Timestamp - p1.Timestamp // second
+	dDistance := haversine(p1, p2) // killometer
+	velocity := velocity(dTime, dDistance) // km/h
+
+	switch{
+		case velocity > 100:
+			return -1
+		case velocity > 10:
+			timestamp := int64(p1.Timestamp)
+			parsedTime := time.Unix(timestamp, 0)
+
+			if isBefore5am(parsedTime) {
+				return structs_and_constants.Moving_before5_fare * dDistance
+			} else {
+				return structs_and_constants.Moving_after5_fare * dDistance
+			}
+		default:
+			return structs_and_constants.Idle_fare * dTime
+	}
 }
--- project/structs_and_constants/structs_and_constants.go
+++ project/structs_and_constants/structs_and_constants.go
@@ -2,8 +2,15 @@ package structs_and_constants
 
 
 const (
-    Buffer_size     =       1000 * 1000
-    R_earth         =       6371
+    Buffer_size             =       1000 * 1000
+
+    R_earth                 =       6371
+
+    Starting_fare           =       1.30
+    Minimum_fare            =       3.47
+    Idle_fare               =       11.90
+    Moving_before5_fare     =       1.30
+    Moving_after5_fare      =       0.74
 )
 
 
@@ -11,7 +18,7 @@ type Point struct {
     Id_delivery int64
     Lat float64
     Lng float64
-    Timestamp int64
+    Timestamp float64
 }
 
 type Output_data struct {
